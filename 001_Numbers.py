39da2528c0abcbd5b71e090fb68092290e5b8578
PJH <woghd8503@naver.com>
2021-07-17 22:43:21 +0900

    Numbers

---
+++ 001_Numbers.py
@@ -0,0 +1,50 @@
+# Integher 정수
+a = 123
+a = -178
+a = 0
+
+# Floating-point 실수
+a= 1.2
+a = -3.45
+
+a = 4.24E10
+a = 4.24e-10
+
+# Octal 8진수
+a = 0o177
+
+# Hexadecimal 16진수
+a = 0x8ff
+b = 0xABC
+
+# Operation 사칙연산
+a = 3
+b = 4
+c = a + b
+print(c)
+
+c = a * b
+print(c)
+
+c = a / b
+print(c)
+
+# **Operation 제곱 곱하기
+a = 3
+b = 4
+c = a ** b
+print(c)
+
+# %Operation 나머지값
+a = 7 % 3
+print(a)
+
+a = 3 % 7
+print(a)
+
+# //Operation 몫을 반환
+a = 7 / 4
+print(a)
+
+a = 7 // 4
+print(a)
