a845fa6e7d31202b1465af4624b447d8d62c52bd
PJH <woghd8503@naver.com>
2021-07-18 23:29:09 +0900

    Bool

---
+++ 007_Bool.py
@@ -0,0 +1,45 @@
+# Bool
+a = True
+print(a)
+b = False
+print(b)
+
+1 == 1
+print(1 == 1)
+
+2 < 1
+print(2 < 1)
+
+# Bool 참과 거짓
+a = [1, 2, 3, 4]
+while a:
+    print(a.pop())
+
+if[]:
+    print("참")
+else:
+    print("거짓")
+
+if[1, 2, 3]:
+    print("참")
+else:
+    print("거짓")
+
+# Bool 불연산
+bool('python')
+print(bool('python'))
+
+bool('')
+print(bool(''))
+
+bool([1,2,3])
+print(bool([1,2,3]))
+
+bool([])
+print(bool([]))
+
+bool(0)
+print(bool(0))
+
+bool(3)
+print(bool(3))
