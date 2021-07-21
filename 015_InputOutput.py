378f6e0ec93b74c6bbe97417c48916cfb6515993
PJH <woghd8503@naver.com>
2021-07-21 22:33:44 +0900

    InputOutput

---
+++ 015_InputOutput.py
@@ -0,0 +1,20 @@
+# IO
+number = input("숫자를 입력하세요: ")
+
+print(number)
+
+# IO print
+a = 123
+print(a)
+a = "Python"
+print(a)
+a = [1, 2, 3]
+print(a)
+
+# 큰 따옴표(")로 둘러싸인 문자열은 + 연산과 동일하다
+print("life" "is " "too short")
+
+print("life" + "is" + "too short")
+
+# 문자열 띄어쓰기는 콤마로 한다
+print("life", "is", "too short")
